e9a1fa3e97e2c9e863465865a4e4a58cecb21b08
luoguoling <luoguoling@gmail.com>
2014-04-08 22:30:55 +0800

    python2.4 md5
    python2.4 md5

--- md5directory.py
+++ md5directory.py
@@ -1,18 +1,33 @@
-__author__ = 'Administrator'
-#coding:utf-8
-import os,smtplib,sys,filecmp
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import hashlib,io
+#python2.4
+import os,sys,filecmp,smtplib
+#from email.mime.multipart import MIMEMultipart
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+#from email.mime.text import MIMEText
+from pickle import dump
+#import io
+try:
+	from hashlib import md5
+except:
+	from md5 import md5
 def find_ip():
-    '''查出ip地址'''
     ip = os.popen("/sbin/ip a|grep 'global eth0'").readlines()[0].split()[1].split("/")[0]
     if "192.168." in ip:
         ip = os.popen("/sbin/ip a|grep 'global eth1'").readlines()[0].split()[1].split("/")[0]
     return ip
+def calMd5(files):
+    m = md5()
+#    file = io.FileIO(files,'r')
+    file1 = file(files,'r')
+    bytes = file1.read(2048)
+    while(bytes != ''):
+        m.update(bytes)
+        bytes = file1.read(1024)
+    file1.close()
+    md5value = m.hexdigest()
+    return md5value
 def sendMail(info):
-    '''邮件发送'''
-    you = "luoguoling@mokylin.com"
+    you = "992975991@qq.com"
     me = 'lgl15984@163.com'
     mail_host = "smtp.163.com"
     mail_user = 'lgl15984'
@@ -31,22 +46,11 @@ def sendMail(info):
     s.login(mail_user,mail_pass)
     s.sendmail(me, you, msg.as_string())
     s.quit()
-def calMd5(files):
-    '''计算md5值'''
-    m = hashlib.md5()
-    file = io.FileIO(files,'r')
-    bytes = file.read(2048)
-    while(bytes != b''):
-        m.update(bytes)
-        bytes = file.read(1024)
-    file.close()
-    md5value = m.hexdigest()
-    return md5value
 def truncatefile():
-        f = open('b.txt','w+')
-        f.truncate()
-        f.close()
-def findDirectory(path):
+	f = open('b.txt','w+')
+	f.truncate()
+	f.close()
+def findDirectoryOld(path):
         for root,subFolders,files in os.walk(path):
                 if 'weblog' in subFolders:
                         subFolders.remove('weblog')
@@ -59,36 +63,73 @@ def findDirectory(path):
 #                       if not os.path.isdir(filepath):
 #                       temp = sys.stdout
 #                       sys.stdout = open('a.txt','w')
-#                       f = open('b.txt','w')
-#                       f.truncate()
-#                       os.remove('b.txt')
+                        f = open('a.txt','a+')
+#                       dump(os.path.join(root,filespath),f)
 #                       f.close()
-#                       sys.stdout = f
-                        so = file('b.txt','a+')
-                        os.dup2(so.fileno(),sys.stdout.fileno())
-                        print  os.path.join(root,filespath), calMd5(filepath)
-def comparefile():
-    flag = filecmp.cmp(r'a.txt',r'b.txt')
-#       print flag
-    ip = find_ip()
-    if flag == False:
-            sendMail('%s the file i changed' % ip)
+#                       a = []
+#                       b = []
+                        for i in os.path.join(root,filespath).split('\n'):
+                                f.write(i),
+                                for j in calMd5(filepath).split('\n'):
+                                        f.write('\t')
+                                        f.write(j),
+                                        f.write('\n')
+                                        f.close()
+def createtxt():
+    if os.path.isfile('a.txt'):
+	pass
     else:
-            print "the file is change"
+        findDirectoryOld(path)
+def findDirectory(path):
+        for root,subFolders,files in os.walk(path):
+                if 'weblog' in subFolders:
+                        subFolders.remove('weblog')
+                for filespath in files:
+                        if 'laodao_ynvng_config.php' in files:
+                                files.remove('laodao_ynvng_config.php')
+#                       print os.path.join(root,filespath)
+                        filepath = os.path.join(root,filespath)
+#                       return filepath
+#                       if not os.path.isdir(filepath):
+#			temp = sys.stdout
+#			sys.stdout = open('a.txt','w')
+			f = open('b.txt','a+')
+#			dump(os.path.join(root,filespath),f)
+#			f.close()
+#			a = []
+#			b = []
+			for i in os.path.join(root,filespath).split('\n'):
+				f.write(i),
+				for j in calMd5(filepath).split('\n'):
+					f.write('\t')
+					f.write(j),
+					f.write('\n')
+					f.close()
+#			f.truncate()
+#			os.remove('b.txt')
+#			f.close()
+#			sys.stdout = f
+#			so = file('b.txt','w+')
+#			os.dup2(so.fileno(),sys.stdout.fileno())
+#                        print  os.path.join(root,filespath), calMd5(filepath)
+#			so.close()
+
+def comparefile():
+	flag = filecmp.cmp(r'a.txt',r'b.txt')
+#	print flag
+#	print flag
+#	print flag
+	ip = find_ip()
+	if flag == False:
+#		pass
+		sendMail('%s the file i changed' % ip)
+	else:
+#		sendMail('%s the file not changed' % ip)
+		pass
 if __name__ == '__main__':
-        path = '/data/www'
-        ip = find_ip()
-        truncatefile()
-        findDirectory(path)
-        comparefile()
-
-
-
-
-
-
-
-
-
-
-
+	path = '/data/www'
+	ip = find_ip()
+	createtxt()
+	truncatefile()
+	findDirectory(path)
+	comparefile()
